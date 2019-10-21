ca00126bbb926d238aaf8f592b40c3054a200790
Jemshit Iskenderov <jemshit@mail.com>
2019-10-21 23:03:42 +0300

    StackUsingSinglyLinkedList

--- src/main/kotlin/stack/StackUsingSinglyLinkedList.kt
+++ src/main/kotlin/stack/StackUsingSinglyLinkedList.kt
@@ -1,5 +1,27 @@
 package stack
 
-class StackUsingSinglyLinkedList<T : Any> {
+import linked_list.SinglyLinkedList
 
+class StackUsingSinglyLinkedList<T : Any> {
+    private val items = SinglyLinkedList<T>()
+
+    fun size(): Int = items.size
+
+    fun isEmpty(): Boolean = items.isEmpty()
+
+    fun push(item: T) {
+        items.addFirst(item)
+    }
+
+    fun pop(): T {
+        if (isEmpty())
+            throw IllegalAccessException()
+        return items.removeFirst()
+    }
+
+    fun peek(): T {
+        if (isEmpty())
+            throw IllegalAccessException()
+        return items.peekFirst()
+    }
 }
