6a081684107213a105775f28797dbe732dbb4aba
Jemshit Iskenderov <jemshit@mail.com>
2019-10-21 23:01:44 +0300

    StackUsingDoublyLinkedList

--- src/main/kotlin/stack/StackUsingDoublyLinkedList.kt
+++ src/main/kotlin/stack/StackUsingDoublyLinkedList.kt
@@ -1,5 +1,27 @@
 package stack
 
-class StackUsingDoublyLinkedList<T : Any> {
+import linked_list.DoublyLinkedList
 
+class StackUsingDoublyLinkedList<T : Any> {
+    private val items = DoublyLinkedList<T>()
+
+    fun size(): Int = items.size
+
+    fun isEmpty(): Boolean = items.isEmpty()
+
+    fun push(item: T) {
+        items.addLast(item)
+    }
+
+    fun pop(): T {
+        if (isEmpty())
+            throw IllegalAccessException()
+        return items.removeLast()
+    }
+
+    fun peek(): T {
+        if (isEmpty())
+            throw IllegalAccessException()
+        return items.peekLast()
+    }
 }
---
+++ src/test/kotlin/stack/StackUsingDoublyLinkedListTest.kt
@@ -0,0 +1,72 @@
+package stack
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class StackUsingDoublyLinkedListTest{
+    private lateinit var stack: StackUsingDoublyLinkedList<Int>
+
+    @BeforeEach
+    fun beforeEach() {
+        stack = StackUsingDoublyLinkedList<Int>()
+    }
+
+    @Test
+    fun `test empty Stack`() {
+        assertTrue(stack.isEmpty())
+        assertEquals(stack.size(), 0)
+    }
+
+    @Test
+    fun `test pop on empty`() {
+        org.junit.jupiter.api.assertThrows<Exception> {
+            stack.pop()
+        }
+    }
+
+    @Test
+    fun `test peek on empty`() {
+        org.junit.jupiter.api.assertThrows<Exception> {
+            stack.peek()
+        }
+    }
+
+    @Test
+    fun `test push`() {
+        stack.push(2)
+        assertEquals(stack.size(), 1)
+    }
+
+    @Test
+    fun `test peek`() {
+        stack.push(2)
+        assertTrue(stack.peek() == 2)
+        assertEquals(stack.size(), 1)
+    }
+
+    @Test
+    fun `test pop`() {
+        stack.push(2)
+        assertTrue(stack.pop() == 2)
+        assertEquals(stack.size(), 0)
+    }
+
+    @Test
+    fun `test exhaustively`() {
+        assertTrue(stack.isEmpty())
+        stack.push(1)
+        assertTrue(!stack.isEmpty())
+        stack.push(2)
+        assertEquals(stack.size(), 2)
+        assertTrue(stack.peek() == 2)
+        assertEquals(stack.size(), 2)
+        assertTrue(stack.pop() == 2)
+        assertEquals(stack.size(), 1)
+        assertTrue(stack.peek() == 1)
+        assertEquals(stack.size(), 1)
+        assertTrue(stack.pop() == 1)
+        assertEquals(stack.size(), 0)
+        assertTrue(stack.isEmpty())
+    }
+}
